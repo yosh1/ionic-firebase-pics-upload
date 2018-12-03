c114eaca524ed4072a0d17432ed548f6eb57686b
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-03 22:23:25 +0900

    [Update] environment.ts

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController, normalizeURL } from 'ionic-angular';
+
+import { ImagePicker } from '@ionic-native/image-picker';
+import { Crop } from '@ionic-native/crop';
+
+import { FirebaseService } from '../service/firebase.service';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,78 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public imagePicker: ImagePicker,
+    public cropService: Crop,
+    public toastCtrl: ToastController,
+    public firebaseService: FirebaseService
+  ) {}
 
+  openImagePickerCrop(){
+    this.imagePicker.hasReadPermission().then(
+      (result) => {
+        if(result == false){
+          // no callbacks required as this opens a popup which returns async
+          this.imagePicker.requestReadPermission();
+        }
+        else if(result == true){
+          this.imagePicker.getPictures({
+            maximumImagesCount: 1
+          }).then(
+            (results) => {
+              for (var i = 0; i < results.length; i++) {
+                this.cropService.crop(results[i], {quality: 75}).then(
+                  newImage => {
+                    this.uploadImageToFirebase(newImage);
+                  },
+                  error => console.error("Error cropping image", error)
+                );
+              }
+            }, (err) => console.log(err)
+          );
+        }
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  openImagePicker(){
+    this.imagePicker.hasReadPermission().then(
+      (result) => {
+        if(result == false){
+          // no callbacks required as this opens a popup which returns async
+          this.imagePicker.requestReadPermission();
+        }
+        else if(result == true){
+          this.imagePicker.getPictures({
+            maximumImagesCount: 1
+          }).then(
+            (results) => {
+              for (var i = 0; i < results.length; i++) {
+                this.uploadImageToFirebase(results[i]);
+              }
+            }, (err) => console.log(err)
+          );
+        }
+      }, (err) => {
+        console.log(err);
+      });
+  }
+
+  uploadImageToFirebase(image){
+    image = normalizeURL(image);
+
+    //uploads img to firebase storage
+    this.firebaseService.uploadImage(image)
+    .then(photoURL => {
+      
+      let toast = this.toastCtrl.create({
+        message: 'Image was updated successfully',
+        duration: 3000
+      });
+      toast.present();
+      })
   }
 
 }
